4f24cdfabc082964719d97e3c26aad9eddc40219
Vinay13510 <117732497+Vinay13510@users.noreply.github.com>
2022-11-08 17:28:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText editText;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn);
+        editText=findViewById(R.id.etext);
+        textView = findViewById(R.id.tview2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = editText.getText().toString();
+                int kg = Integer.parseInt(s);
+                double pound = 2.205*kg;
+                textView.setText(" Rey Karuvu Pounds Value "+kg+" is: "+pound);
+            }
+        });
+
+    }
+}
